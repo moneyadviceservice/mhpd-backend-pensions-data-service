5a2c0cc65061675b3ac7726a2d0873b4c4e7d454
Hasan Kasamali <HA40163128@wipro.com>
2024-04-18 15:05:10 +0100

    #22218 Added token controller boilerplate and unit test project in new folder structure

---
+++ TokenIntegrationService/tests/TokenIntegrationServiceUnitTests/TokenIntegrationServiceUnitTests.cs
@@ -0,0 +1,12 @@
+namespace TokenIntegrationServiceUnitTests
+{
+    public class TokenIntegrationServiceUnitTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(1==1);
+
+        }
+    }
+}

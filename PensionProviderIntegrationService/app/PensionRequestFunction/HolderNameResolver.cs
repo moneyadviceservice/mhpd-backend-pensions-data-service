b3c3f102433d000ee27ae98d623c06251c86eeb8
Hasan Kasamali <hasan.kasamali@wipro.com>
2024-08-08 09:15:47 +0000

    Merged PR 4327: Move the pdp view data url into app setttings
    Move the pdp view data url into app setttings

--- PensionProviderIntegrationService/app/PensionRequestFunction/HolderNameResolver.cs
+++ PensionProviderIntegrationService/app/PensionRequestFunction/HolderNameResolver.cs
@@ -3,11 +3,10 @@ namespace PensionRequestFunction
 {
     public static class HolderNameResolver
     {
-        private const string ViewDataUrl = "https://pdpviewdataservicedemulator.azurewebsites.net/view-data/";
-
         public static string GetViewDataUrl(string holderNameGuid )
         {
-            return ViewDataUrl;
+            return Environment.GetEnvironmentVariable("PdpViewDataUrl")!;
         }
     }
 }
+

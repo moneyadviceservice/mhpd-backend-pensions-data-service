c798f99d1407e2e01f34d6d14ac307d28f2420c4
Frederic Jonquieres <frederic.jonquieres@solirius.com>
2024-09-19 10:47:36 +0000

    Merged PR 4546: Adding the Models to accept redirect details request simple response
    Adding the Models to accept redirect details request simple response
    
    Related work items: #25046

---
+++ services/CommonServices/MhpdCommon/Models/MHPDModels/RedirectResponseModel.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+
+namespace MhpdCommon.Models.MHPDModels;
+
+public class RedirectResponseModel
+{
+    [JsonProperty("redirectTargetUrl")]
+    public string? RedirectTargetUrl { get; set; }
+
+    [JsonProperty("rqp")]
+    public string? Rqp { get; set; }
+
+    [JsonProperty("scope")]
+    public string? Scope { get; set; }
+
+    [JsonProperty("responseType")]
+    public string? ResponseType { get; set; }
+
+    [JsonProperty("prompt")]
+    public string? Prompt { get; set; }
+
+    [JsonProperty("service")]
+    public string? Service { get; set; }
+
+    [JsonProperty("codeChallengeMethod")]
+    public string? CodeChallengeMethod { get; set; }
+
+    [JsonProperty("codeChallenge")]
+    public string? CodeChallenge { get; set; }
+
+    [JsonProperty("codeVerifier")]
+    public string? CodeVerifier { get; set; }
+
+    [JsonProperty("requestId")]
+    public string? RequestId { get; set; }
+}
+
---
+++ services/CommonServices/MhpdCommon/Models/MessageBodyModels/RedirectRequestPayload.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+
+namespace MhpdCommon.Models.MessageBodyModels;
+
+public class RedirectRequestPayload
+{
+    [JsonProperty("redirectPurpose")]
+    public string? RedirectPurpose { get; set; }
+
+    [JsonProperty("iss")]
+    public string? Iss { get; set; }
+
+    [JsonProperty("userSessionId")]
+    public string? UserSessionId { get; set; }
+}

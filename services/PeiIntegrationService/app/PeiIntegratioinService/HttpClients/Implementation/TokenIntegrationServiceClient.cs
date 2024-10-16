17907af693988c1e419d2fc380258abd8d83b97e
Jerry Louis <jerry.louis@solirius.com>
2024-10-16 11:57:51 +0000

    Merged PR 4706: Added a random guid for x-request-id
    Added a random guid for x-request-id
    
    Related work items: #28795

--- services/PeiIntegrationService/app/PeiIntegratioinService/HttpClients/Implementation/TokenIntegrationServiceClient.cs
+++ services/PeiIntegrationService/app/PeiIntegratioinService/HttpClients/Implementation/TokenIntegrationServiceClient.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using System.Text.Json;
+using MhpdCommon.Constants;
 using PeiIntegrationService.HttpClients.Interfaces;
 using PeiIntegrationService.Models;
 using PeiIntegrationService.Models.TokenIntegrationService;
@@ -21,16 +22,21 @@ namespace PeiIntegrationService.HttpClients.Implementation
             _cdaTokenServicesEndpoint = _configuration[Constants.HttpEndpoints.CdaTokenServicesEndpoint]!;
             _mapsTokenIntegrationServiceEndpoint = _configuration[Constants.HttpEndpoints.TokenIntegrationServiceEndpoint]!;
         }
+        
         public async Task<TokenIntegrationResponseModel> PostRpt(TokenIntegrationServiceRequestModel request)
         {
             var client = _httpClientFactory.CreateClient(Constants.HttpClients.TokenIntegrationService);
             client.BaseAddress = new Uri(_mapsTokenIntegrationServiceEndpoint);
+            
+            // Add request ID header
+            // Note: this should be passed in from the entrypoint ot the backend services
+            client.DefaultRequestHeaders.Add(HeaderConstants.RequestId, Guid.NewGuid().ToString());
 
             request.As_Uri = _cdaTokenServicesEndpoint; // <<<======== this should come in through via wwwAuthenticate header as_uri
             var payload = JsonSerializer.Serialize(request);
 
             var content = new StringContent(payload, Encoding.UTF8, "application/json");
-            var responseTokenInt = await client!.PostAsync(Constants.RequestRoutes.Rpt, content);
+            var responseTokenInt = await client.PostAsync(Constants.RequestRoutes.Rpt, content);
             var result = responseTokenInt.Content.ReadFromJsonAsync<TokenIntegrationResponseModel>().Result;
             return result!;
         }

dc090ebefa7d1ac8b2863a424b2a43035f81d143
Jerry Louis <jerry.louis@solirius.com>
2025-01-28 16:24:01 +0000

    Merged PR 5296: Updated local.tf
    Updated local.tf
    
    Related work items: #31724

--- shared-infrastructure/components/apim/local.tf
+++ shared-infrastructure/components/apim/local.tf
@@ -4,7 +4,7 @@ locals {
   cda-backend-service       = "https://maps-api-management-${var.env}.azure-api.net/cda-emulator"
   view-data-backend-service = "@((string)context.Variables[\"backendUrl\"])"
   certificate-id            = "PdpMtls"
-  backendUrl                = "@(context.Request.Headers.GetValueOrDefault(\"provider\"))"
+  backendUrl                = "@(context.Request.Headers.GetValueOrDefault(\"providerUrl\"))"
 
   api_management_logger_id = var.env == "prod" ? "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/maps-apim-${var.env}/providers/Microsoft.ApiManagement/service/maps-api-management-${var.env}/loggers/apim-logger-prod" : "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/maps-apim-${var.env}/providers/Microsoft.ApiManagement/service/maps-api-management-${var.env}/loggers/apim-logger-nonprod"
 }

9d1ae476de3141bdd314188dcfb72938c045f522
Fola Sonoiki <fola.sonoiki@solirius.com>
2025-01-29 16:09:46 +0000

    Merged PR 5304: 29490-Jwk-Caching-Connx-String
    Related work items: #29490

--- shared-infrastructure/components/app-services/token-integration-service.tf
+++ shared-infrastructure/components/app-services/token-integration-service.tf
@@ -30,6 +30,7 @@ module "token_integration_service" {
     "CosmosIntegrationConfiguration__DatabaseId"               = "mhpd-integration-layer"
     "CosmosIntegrationConfiguration__HolderNameCacheContainer" = "mhpdholderNameViewConfigurationData"
     "CosmosIntegrationConfiguration__JwkCacheContainer"        = "jwkUriEmulatorData"
+    "CosmosDBConnectionString"                                 = "AccountEndpoint=https://${var.product}-cosmos-${var.env}.documents.azure.com:443/;AccountKey=${data.azurerm_cosmosdb_account.this.primary_key}"
   }
   tags = {}
 }

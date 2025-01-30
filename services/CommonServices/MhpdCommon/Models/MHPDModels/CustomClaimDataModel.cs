a20230f811ec9b361b3ecb0220b4cd2d11acdbea
Fola Sonoiki <fola.sonoiki@solirius.com>
2025-01-30 14:13:00 +0000

    Merged PR 5308: 30500-Jwt-Signature-Unhappy-Paths-Models
    Added models to support Token signature unhappy path
    
    Related work items: #30500

--- services/CommonServices/MhpdCommon/Models/MHPDModels/CustomClaimDataModel.cs
+++ services/CommonServices/MhpdCommon/Models/MHPDModels/CustomClaimDataModel.cs
@@ -3,5 +3,8 @@ namespace MhpdCommon.Models.MHPDModels;
 public class CustomClaimDataModel
 {
     public string? Name { get; set; }
+
     public string? Data { get; set; }
+
+    public string? Subject { get; set; }
 }

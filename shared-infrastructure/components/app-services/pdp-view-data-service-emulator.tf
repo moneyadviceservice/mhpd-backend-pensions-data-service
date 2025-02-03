cb5bc2de083b5572ec45bbd280ccc992ef5d76ff
Fola Sonoiki <fola.sonoiki@solirius.com>
2025-02-03 14:13:54 +0000

    Merged PR 5336: 30500-Jwt-Signature-Unhappy-Paths-Appsettings
    Related work items: #30498, #30500, #30503

--- shared-infrastructure/components/app-services/pdp-view-data-service-emulator.tf
+++ shared-infrastructure/components/app-services/pdp-view-data-service-emulator.tf
@@ -17,7 +17,7 @@ module "pdp_view_data_service_emulator" {
   }]
   app_settings = {
     "JwtSettings__ExpiryInSeconds"                           = "600"
-    "JwtSettings__PrivateKey"                                = data.azurerm_key_vault_secret.CDA_service_emulator_private_key.value
+    "JwtSettings__PrivateKey"                                = data.azurerm_key_vault_secret.cda_service_emulator_private_key.value
     "JwtSettings__Audience"                                  = "https://pdp/ig/token"
     "JwtSettings__Issuer"                                    = "https://emulators.maps.org.uk/am/oauth2"
     "JwtSettings__Kid"                                       = data.azurerm_key_vault_secret.jwt_settings_kid.value

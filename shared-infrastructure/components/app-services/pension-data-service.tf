201f68a8391c052a90e1b28913e39e9ddb487484
Fola Sonoiki <fola.sonoiki@solirius.com>
2025-03-21 12:50:57 +0000

    Merged PR 5664: 32921-Claims-Gathering-Redirect-AppSettings
    Related work items: #32916

--- shared-infrastructure/components/app-services/pension-data-service.tf
+++ shared-infrastructure/components/app-services/pension-data-service.tf
@@ -15,11 +15,11 @@ module "pension_data_service" {
 
   app_settings = {
     "CommonServiceBusConfiguration__OutboundQueue"          = "pensions-retrieval-job"
-    "OutboundQueue"                                         = "pensions-retrieval-job"
     "ServiceBusConnectionString"                            = data.azurerm_servicebus_namespace.this.default_primary_connection_string
     "PensionRetrievalServiceUrl"                            = "https://maps-api-management-${var.env}.azure-api.net/pension-retrieval-service/"
     "RetrievedPensionsServiceUrl"                           = "https://maps-api-management-${var.env}.azure-api.net/retrieved-pensions-record-service/"
     "tokenIntegrationServiceUrl"                            = "https://maps-api-management-${var.env}.azure-api.net/token-integration-service/"
+    "MapsCdaServiceUrl"                                     = "https://maps-api-management-${var.env}.azure-api.net/maps-cda-service/"
     "WEBSITE_ENABLE_SYNC_UPDATE_SITE"                       = true
     "APPLICATIONINSIGHTS_CONNECTION_STRING"                 = "InstrumentationKey=${module.pension_data_service.instrumentation_key};IngestionEndpoint=https://uksouth-1.in.applicationinsights.azure.com/;LiveEndpoint=https://uksouth.livediagnostics.monitor.azure.com/;ApplicationId=${module.pension_data_service.app_insights_app_id}"
     "PeiRetrievalDuration"                                  = "60"
--- shared-infrastructure/components/app-services/token-integration-service.tf
+++ shared-infrastructure/components/app-services/token-integration-service.tf
@@ -14,25 +14,25 @@ module "token_integration_service" {
   subnet_id               = local.subnet_id
 
   app_settings = {
-    "APPINSIGHTS_INSTRUMENTATIONKEY"                           = module.token_integration_service.instrumentation_key
-    "APPINSIGHTS_PROFILERFEATURE_VERSION"                      = "1.0.0"
-    "APPINSIGHTS_SNAPSHOTFEATURE_VERSION"                      = "1.0.0"
-    "APPLICATIONINSIGHTS_CONNECTION_STRING"                    = "InstrumentationKey=${module.token_integration_service.instrumentation_key};IngestionEndpoint=https://uksouth-1.in.applicationinsights.azure.com/;LiveEndpoint=https://uksouth.livediagnostics.monitor.azure.com/;ApplicationId=${module.token_integration_service.app_insights_app_id}"
-    "APPLICATIONINSIGHTS_ENABLESQLQUERYCOLLECTION"             = "disabled"
-    "ApplicationInsightsAgent_EXTENSION_VERSION"               = "~2"
-    "DISABLE_APPINSIGHTS_SDK"                                  = "disabled"
-    "DiagnosticServices_EXTENSION_VERSION"                     = "~3"
-    "IGNORE_APPINSIGHTS_SDK"                                   = "disabled"
-    "InstrumentationEngine_EXTENSION_VERSION"                  = "disabled"
-    "SnapshotDebugger_EXTENSION_VERSION"                       = "disabled"
-    "WEBSITE_ENABLE_SYNC_UPDATE_SITE"                          = "true"
-    "XDT_MicrosoftApplicationInsights_BaseExtensions"          = "disabled"
-    "XDT_MicrosoftApplicationInsights_Mode"                    = "recommended"
-    "XDT_MicrosoftApplicationInsights_PreemptSdk"              = "disabled"
-    "CdaServiceUrl"                                            = "https://maps-api-management-${var.env}.azure-api.net/cda-integration-external/"
-    "CosmosIntegrationConfiguration__DatabaseId"               = "mhpd-integration-layer"
-    "CosmosIntegrationConfiguration__JwkCacheContainer"        = "jwkUriEmulatorData"
-    "CosmosDBConnectionString"                                 = "AccountEndpoint=https://${var.product}-cosmos-${var.env}.documents.azure.com:443/;AccountKey=${data.azurerm_cosmosdb_account.this.primary_key}"
+    "APPINSIGHTS_INSTRUMENTATIONKEY"                    = module.token_integration_service.instrumentation_key
+    "APPINSIGHTS_PROFILERFEATURE_VERSION"               = "1.0.0"
+    "APPINSIGHTS_SNAPSHOTFEATURE_VERSION"               = "1.0.0"
+    "APPLICATIONINSIGHTS_CONNECTION_STRING"             = "InstrumentationKey=${module.token_integration_service.instrumentation_key};IngestionEndpoint=https://uksouth-1.in.applicationinsights.azure.com/;LiveEndpoint=https://uksouth.livediagnostics.monitor.azure.com/;ApplicationId=${module.token_integration_service.app_insights_app_id}"
+    "APPLICATIONINSIGHTS_ENABLESQLQUERYCOLLECTION"      = "disabled"
+    "ApplicationInsightsAgent_EXTENSION_VERSION"        = "~2"
+    "DISABLE_APPINSIGHTS_SDK"                           = "disabled"
+    "DiagnosticServices_EXTENSION_VERSION"              = "~3"
+    "IGNORE_APPINSIGHTS_SDK"                            = "disabled"
+    "InstrumentationEngine_EXTENSION_VERSION"           = "disabled"
+    "SnapshotDebugger_EXTENSION_VERSION"                = "disabled"
+    "WEBSITE_ENABLE_SYNC_UPDATE_SITE"                   = "true"
+    "XDT_MicrosoftApplicationInsights_BaseExtensions"   = "disabled"
+    "XDT_MicrosoftApplicationInsights_Mode"             = "recommended"
+    "XDT_MicrosoftApplicationInsights_PreemptSdk"       = "disabled"
+    "CdaServiceUrl"                                     = "https://maps-api-management-${var.env}.azure-api.net/cda-integration-external/"
+    "CosmosIntegrationConfiguration__DatabaseId"        = "mhpd-integration-layer"
+    "CosmosIntegrationConfiguration__JwkCacheContainer" = "jwkUriEmulatorData"
+    "CosmosDBConnectionString"                          = "AccountEndpoint=https://${var.product}-cosmos-${var.env}.documents.azure.com:443/;AccountKey=${data.azurerm_cosmosdb_account.this.primary_key}"
   }
   tags = {}
 }

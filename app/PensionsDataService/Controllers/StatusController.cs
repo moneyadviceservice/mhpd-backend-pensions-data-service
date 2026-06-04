9ea3a0c41460ada1a1ec4d0f7077709b54979b2f
Fola Sonoiki <fola.sonoiki@solirius.com>
2026-06-04 13:32:15 +0100

    Added status monitoring endpoint

---
+++ app/PensionsDataService/Controllers/StatusController.cs
@@ -0,0 +1,30 @@
+﻿using MhpdCommon.Constants.HttpClient;
+using MhpdCommon.Models.MHPDModels;
+using MhpdCommon.SharedHttpClient;
+using MhpdCommon.Utils;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PensionsDataService.Controllers;
+
+public class StatusController(IServiceStatusProvider statusProvider, IServiceStatusClient statusClient) : ControllerBase
+{
+    public async Task<IActionResult> Status()
+    {
+        var servicesToMonitor = new[]
+        {
+            HttpClientNames.PensionRetrievalService
+        };
+
+        var status = statusProvider.GetServiceStatus();
+
+        var serviceStatuses = new List<ServiceStatus> { status };
+
+        var statusTasks = servicesToMonitor.Select(service => statusClient.GetServiceStatusAsync(service));
+        ServiceStatus[] downstreamStatusList = await Task.WhenAll(statusTasks);
+
+        serviceStatuses.AddRange(downstreamStatusList);
+
+
+        return Ok(serviceStatuses);
+    }
+}

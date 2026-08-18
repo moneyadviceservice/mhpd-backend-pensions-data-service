f788b29c9735598e45c21f81473be8b147430c3c
Jaswanth Chigurupati <jaswanth.chigurupati@maps.org.uk>
2026-08-18 08:56:40 +0000

    Merged PR 10852: Private endpoints and Redis

--- mhpd-landingzones/components/pensions-data-app-service/data.tf
+++ mhpd-landingzones/components/pensions-data-app-service/data.tf
@@ -7,3 +7,24 @@ data "azurerm_servicebus_namespace" "mhpd" {
   name                = "sbns-${var.product}-${var.env}-uks"
   resource_group_name = "rg-${var.product}-${var.env}-uksouth"
 }
+
+data "azurerm_key_vault" "mhpd" {
+  name                = "kv-${var.product}-${var.env}-uks"
+  resource_group_name = "rg-${var.product}-${var.env}-uksouth"
+}
+
+data "azurerm_key_vault_secret" "redis_endpoint" {
+  name         = "redis-endpoint"
+  key_vault_id = data.azurerm_key_vault.mhpd.id
+}
+
+data "azurerm_key_vault_secret" "redis_primary_key" {
+  name         = "redis-primary-key"
+  key_vault_id = data.azurerm_key_vault.mhpd.id
+}
+
+data "azurerm_private_dns_zone" "app_service" {
+  count               = local.pe_enabled ? 1 : 0
+  name                = "privatelink.azurewebsites.net"
+  resource_group_name = "rg-${var.product}-${var.env}-uksouth"
+}
--- mhpd-landingzones/components/pensions-data-app-service/main.tf
+++ mhpd-landingzones/components/pensions-data-app-service/main.tf
@@ -13,14 +13,14 @@ resource "azurerm_service_plan" "this" {
 }
 
 resource "azurerm_linux_web_app" "this" {
-  name                          = local.app_name
+  name                          = lower(local.app_name)
   resource_group_name           = local.resource_group_name
   location                      = azurerm_service_plan.this.location
   service_plan_id               = azurerm_service_plan.this.id
   app_settings                  = local.pension_data_app_settings
   client_affinity_enabled       = true
   https_only                    = true
-  public_network_access_enabled = true
+  public_network_access_enabled = false
   virtual_network_subnet_id     = local.enable_vnet_integration ? local.apps_subnet_id : null
   tags                          = {}
 
@@ -34,6 +34,12 @@ resource "azurerm_linux_web_app" "this" {
     name  = local.service_bus_connection_string.name
     type  = local.service_bus_connection_string.type
     value = local.service_bus_connection_string.value
+  }
+
+  connection_string {
+    name  = "RedisConnectionString"
+    type  = "Custom"
+    value = local.redis_connection_string
   }
 
   identity {
@@ -134,7 +140,7 @@ resource "azurerm_linux_web_app_slot" "staging" {
 
   client_affinity_enabled       = true
   https_only                    = true
-  public_network_access_enabled = true
+  public_network_access_enabled = false
   virtual_network_subnet_id     = local.apps_subnet_id
 
   connection_string {
@@ -147,6 +153,12 @@ resource "azurerm_linux_web_app_slot" "staging" {
     name  = local.service_bus_connection_string.name
     type  = local.service_bus_connection_string.type
     value = local.service_bus_connection_string.value
+  }
+
+  connection_string {
+    name  = "RedisConnectionString"
+    type  = "Custom"
+    value = local.redis_connection_string
   }
 
   identity {
@@ -181,8 +193,29 @@ resource "azurerm_linux_web_app_slot" "staging" {
   }
 }
 
+resource "azurerm_private_endpoint" "this" {
+  count               = local.pe_enabled ? 1 : 0
+  name                = "pe-${var.product}-pensions-data-${var.env}-${local.loc}"
+  location            = var.location
+  resource_group_name = local.resource_group_name
+  subnet_id           = local.pe_subnet_id
+
+  private_service_connection {
+    name                           = "psc-${var.product}-pensions-data-${var.env}-${local.loc}"
+    private_connection_resource_id = azurerm_linux_web_app.this.id
+    is_manual_connection           = false
+    subresource_names              = ["sites"]
+  }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+
+  depends_on = [azurerm_linux_web_app.this]
+}
+
 resource "azurerm_application_insights" "this" {
-  name                = local.app_name
+  name                = lower(local.app_name)
   location            = var.location
   resource_group_name = local.resource_group_name
 
@@ -192,6 +225,33 @@ resource "azurerm_application_insights" "this" {
   workspace_id                          = local.logs_workspace_id
   retention_in_days                     = 90
   daily_data_cap_notifications_disabled = true
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+
+resource "azurerm_private_dns_a_record" "app_service" {
+  count               = local.pe_enabled ? 1 : 0
+  name                = lower(local.app_name)
+  zone_name           = data.azurerm_private_dns_zone.app_service[0].name
+  resource_group_name = "rg-${var.product}-${var.env}-uksouth"
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.this[0].private_service_connection[0].private_ip_address]
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_private_dns_a_record" "app_service_scm" {
+  count               = local.pe_enabled ? 1 : 0
+  name                = lower("${local.app_name}.scm")
+  zone_name           = data.azurerm_private_dns_zone.app_service[0].name
+  resource_group_name = "rg-${var.product}-${var.env}-uksouth"
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.this[0].private_service_connection[0].private_ip_address]
 
   lifecycle {
     ignore_changes = [tags]
